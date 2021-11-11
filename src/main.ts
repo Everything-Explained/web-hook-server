c301a4f4da50afa15facba712d20af51fcc81044
Jason <exanix@gmail.com>
2021-11-10 18:18:05 -0800

    feat(main): add color and more info to server startup logs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import config from './config.json';
 import { releaseHook } from './routes/release_hook';
-import { log, logErr } from './utils';
+import { colors, log, logErr } from './utils';
+import { resolve as pathResolve } from 'path';
 
 
 
@@ -27,7 +28,13 @@ server.listen(config.port, '0.0.0.0', (err, address) => {
     logErr(err);
     process.exit(1);
   }
-  log(`Server listening at ${address}`);
+  const c = colors;
+  const workingDir = process.cwd();
+  const productionDir = pathResolve(workingDir, '../_production/web_release/web_client');
+  log('\n\n');
+  log(`${c.bb('WORK Dir:')}\n${c.dw(process.cwd())}\n`);
+  log(`${c.bb('PROD Dir:')}\n${c.dw(productionDir)}\n`);
+  log(`\n\n${c.bw('Server Listening')} ${c.dw('at')} ${c.g(address)}\n\n\n`);
 });
 
 
